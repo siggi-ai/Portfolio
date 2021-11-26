6ae40c7cd1759c2dd6bf13f1e20b500ce6969536
siggi <siegfried.hamm@gmail.com>
2021-11-26 15:11:38 +0100

    making links movie page work

--- app.js
+++ app.js
@@ -29,6 +29,30 @@ app.get("/movies", function(req, res) {
     res.render("movie-page/index.ejs");
 });
 
+app.get("/movie1", function(req, res) {
+    res.render("movie-page/movie1.ejs");
+});
+
+app.get("/movie2", function(req, res) {
+    res.render("movie-page/movie2.ejs");
+});
+
+app.get("/movie3", function(req, res) {
+    res.render("movie-page/movie3.ejs");
+});
+
+app.get("/movie4", function(req, res) {
+    res.render("movie-page/movie4.ejs");
+});
+
+app.get("/movie5", function(req, res) {
+    res.render("movie-page/movie5.ejs");
+});
+
+app.get("/movie6", function(req, res) {
+    res.render("movie-page/movie6.ejs");
+});
+
 app.listen(port, function() {
     console.log(`Listening on ${port}`);
 });
