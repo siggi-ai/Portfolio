ed2bfa753d179ad18d3c21f90e268643f2f15881
siggi <siegfried.hamm@gmail.com>
2021-11-26 14:29:10 +0100

    pictures showing up :)

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require("path");
 const port = 3000;
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
     res.render("index.ejs");
