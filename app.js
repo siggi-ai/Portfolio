a7559ed95bde34d12bff56b200ae9c40336aa62d
siggi <siegfried.hamm@gmail.com>
2021-11-26 16:23:08 +0100

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.set("view engine", "ejs");
