4aecf8953207e3522a3896f4c3b869d2b98fb4fa
siggi <siegfried.hamm@gmail.com>
2021-11-26 11:40:09 +0100

    main page shows up

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const path = require("path");
 const port = 3000;
 const app = express();
 
-// const absolutePath = path.join(__dirname, "public");
-// const static = express.static(absolutePath);
-// app.use(static);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res) {
-    res.send("Try the path /styles.css you will like it...");
+    res.render("index.ejs");
 });
 
 app.listen(port, function() {
-    console.log(`listening on ${port}`);
+    console.log(`Listening on ${port}`);
 });
